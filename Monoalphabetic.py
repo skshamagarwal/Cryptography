a245e73bdd226e266e59399f0c922535c41e844d
skshamagarwal <skshamagarwal@gmail.com>
2021-11-23 11:03:34 +0530

    Monoalphabetic Cipher

--- Monoalphabetic.py
+++ Monoalphabetic.py
@@ -1,27 +1,28 @@
+import random
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+key = ''.join(random.sample(alpha,len(alpha)))
 
 # Encryption
 plain_text = input("Plain Text: ")
+print("Key:", key)
 cipher_text = ''
-k = 3
+
 for char in plain_text:
     if char.upper() in alpha:
         p_idx = alpha.index(char.upper())
-        ct_idx = (p_idx + k)%26
-        cipher_text+=alpha[ct_idx]
+        cipher_text+=key[p_idx]
     else:
         cipher_text+=char
-    
+        
 print("Cipher Text:", cipher_text)
 
 # Decryption
 plain_text = ''
 for char in cipher_text:
     if char.upper() in alpha:
-        ct_idx = alpha.index(char.upper())
-        p_idx = (ct_idx - k)%26
-        plain_text+=alpha[p_idx]
+        ct_idx = key.index(char.upper())
+        plain_text+=alpha[ct_idx]
     else:
         plain_text+=char
-
-print("Dechiphered Text:", plain_text)
+        
+print("Deciphered Text:", plain_text)
